e65aa0688666da98dc18785485ab863a49d2e9b8
keandra <150497342+keandra@users.noreply.github.com>
2024-02-09 16:39:06 +0700

    Update java.js

--- java.js
+++ java.js
@@ -7,3 +7,12 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
